30da9aba4dcbd6b67c05baf1b69b1f9380b88eb0
Tomaslevys <141672471+Tomaslevys@users.noreply.github.com>
2024-06-24 15:46:29 -0300

    .

--- Assets/consecutivamente.cs
+++ Assets/consecutivamente.cs
@@ -13,6 +13,11 @@ public class consecutivamente : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.LeftArrow){
+
+
+        }
+
     }
+   
 }
